7b0d5ace0bbb268adb44090321184755ec326dbe
Schmerb <mike.schmerbeck@gmail.com>
2018-02-02 13:09:15 -0500

    fixed a11y issue with img tag role attr

--- src/icons/party/green-party-icon.js
+++ src/icons/party/green-party-icon.js
@@ -9,6 +9,6 @@ import React from 'react';
 
 export default function GreenPartyIcon(props) {
     return(
-            <img className={props.className} onClick={props.onClick} role="img" alt="Green party logo" aria-label="Green party icon" src="assets/icons/green-party-logo.png" alt=""/>
+            <img className={props.className} onClick={props.onClick} alt="Green party logo" aria-label="Green party icon" src="assets/icons/green-party-logo.png"/>
     );
 }
