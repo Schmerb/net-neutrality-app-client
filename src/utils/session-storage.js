ef2cd9bb40bd23341429e79d3eff646743b6eff2
ripleyaffect <t.ripley.peterson@gmail.com>
2018-02-05 21:51:45 -0500

    Prettify more src

--- src/utils/session-storage.js
+++ src/utils/session-storage.js
@@ -1,25 +1,24 @@
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 //
 //	Session Storage
 //
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 
-
-// * * * * * * * * * * * * * * * 
+// * * * * * * * * * * * * * * *
 //      Map / Candidates
-// * * * * * * * * * * * * * * * 
+// * * * * * * * * * * * * * * *
 
 // saves current store state in session
 export const saveCurrentState = state => {
-    return sessionStorage.setItem('state', JSON.stringify(state));
+  return sessionStorage.setItem('state', JSON.stringify(state));
 };
 
 // returns current state state from session
 export const getPrevState = state => {
-    return JSON.parse(sessionStorage.getItem('state'));
+  return JSON.parse(sessionStorage.getItem('state'));
 };
 
 // removes store state from session
 export const clearMapStore = store => {
-    return sessionStorage.removeItem('store');
+  return sessionStorage.removeItem('store');
 };
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,31 +1,31 @@
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 //
 //	Redux-Form Validations
 //
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 
 export const required = value => (value ? undefined : 'Required');
 
 export const nonEmpty = value =>
-    value.trim() !== '' ? undefined : 'Cannot be empty';
+  value.trim() !== '' ? undefined : 'Cannot be empty';
 
 export const isTrimmed = value =>
-    value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+  value.trim() === value ? undefined : 'Cannot start or end with whitespace';
 
 export const length = length => value => {
-    if (length.min && value.length < length.min) {
-        return `Must be at least ${length.min} characters long`;
-    }
-    if (length.max && value.length > length.max) {
-        return `Must be at most ${length.max} characters long`;
-    }
+  if (length.min && value.length < length.min) {
+    return `Must be at least ${length.min} characters long`;
+  }
+  if (length.max && value.length > length.max) {
+    return `Must be at most ${length.max} characters long`;
+  }
 };
 
 export const matches = field => (value, allValues) =>
-    field in allValues && value.trim() === allValues[field].trim()
-        ? undefined
-        : 'Does not match';
-        
+  field in allValues && value.trim() === allValues[field].trim()
+    ? undefined
+    : 'Does not match';
+
 // Uses a regular expression (regex) to check whether it looks enough like an
 // email address.  Broken down:
 // ^ Matches the start the text
@@ -34,4 +34,4 @@ export const matches = field => (value, allValues) =>
 // \S+ Matches one or more non-whitespace characters after the @
 // $ Matches the end of the text
 export const email = value =>
-    /^\S+@\S+$/.test(value) ? undefined : 'Must be a valid email address';
+  /^\S+@\S+$/.test(value) ? undefined : 'Must be a valid email address';
