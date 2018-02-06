8bdc943464d16766f29b3c64474695dbb4e6be52
ripleyaffect <t.ripley.peterson@gmail.com>
2018-02-05 21:33:27 -0500

    Add route slug to config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 //
 //	Config
 //
-// // // // // // // // // // // // 
+// // // // // // // // // // // //
 
 // PORT
 export const PORT = process.env.PORT || 8080;
 
 // API
-export const API_BASE_URL = 
-    process.env.REACT_APP_API_BASE_URL || "http://192.168.1.113:7000/api/v1";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://192.168.1.113:7000/api/v1';
+
+export const ROUTE_SLUG = process.env.REACT_APP_ROUTE_SLUG || '';
