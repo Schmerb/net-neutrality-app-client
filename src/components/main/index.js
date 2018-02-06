9548fdc7b96ffb68090d40239cf5e82bb07b41da
ripleyaffect <t.ripley.peterson@gmail.com>
2018-02-05 21:20:57 -0500

    Add slug to path

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -8,38 +8,49 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route } from 'react-router-dom';
 
-import LandingPage      from './landing';
-import StatesMap        from './states-map';
-import SourcesPage      from './sources-page';
+import LandingPage from './landing';
+import StatesMap from './states-map';
+import SourcesPage from './sources-page';
 import AboutProjectPage from './about-project-page';
 
-
-
 export class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return(
-            <main role="main" >
-                <div className="container">
-                    <Route exact path="/"    component={LandingPage}/>
-                    <Route exact path="/map" component={StatesMap}/>
-                    <Route path="/sources"   component={SourcesPage}/>
-                    {!this.props.loading ? <Route path="/about-project" component={AboutProjectPage}/> : null}
-                </div> 
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main role="main">
+        <div className="container">
+          <Route
+            exact
+            path={`${process.env.PATH_BASE || ''}/`}
+            component={LandingPage}
+          />
+          <Route
+            exact
+            path={`${process.env.PATH_BASE || ''}/map`}
+            component={StatesMap}
+          />
+          <Route
+            path={`${process.env.PATH_BASE || ''}/sources`}
+            component={SourcesPage}
+          />
+          {!this.props.loading ? (
+            <Route path="/about-project" component={AboutProjectPage} />
+          ) : null}
+        </div>
+      </main>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    hasTouch: state.display.hasTouch,
-    width: state.display.width,
-    height: state.display.height,
-    loading: state.display.loading
+  hasTouch: state.display.hasTouch,
+  width: state.display.width,
+  height: state.display.height,
+  loading: state.display.loading
 });
 
 export default withRouter(connect(mapStateToProps)(Main));
